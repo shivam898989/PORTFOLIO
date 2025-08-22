acc014dc304dd01b2a4b1c405927075edfcac36b
shivam898989 <135410610+shivam898989@users.noreply.github.com>
2025-08-22 19:04:20 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { FaGithub, FaLinkedin, FaDownload } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import './Hero.css';
 
 const Hero = () => {
